9fa1dda9d7070a6575312685b9a12dfd1b458ee0
Unique4070 <uniqueimmaculateita@gmail.com>
2022-09-16 07:04:49 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,97 @@
-#include <stdio.h>
 #include "main.h"
 
+
+
 /**
- * * print_number - Prints a number
- * * @n: The number to print
- * */
+
+ * print_number - prints an integer
+
+ * @n: random integer
+
+ *Return: void
+
+ **/
+
+
 
 void print_number(int n)
+
 {
-	unsigned int num = n;
+
+	char ld, cr;
+
+	int r;
+
+	int ct = 0;
+
+
 
 	if (n < 0)
+
 	{
-			putchar('-');
-			num = -num;
-		}
-	if (num > 9)
+
+		_putchar ('-');
+
+		ld = (char)('0' - (n % 10));
+
+		n = n / -10;
+
+	}
+
+
+
+		else
+
+		{
+
+			ld = (char)((n % 10) + '0');
+
+		n = n / 10;
+
+	}
+
+
+
+	r = 0;
+
+	while (n > 0)
+
 	{
-			print_number(num / 10);
-		}
-	putchar(num % 10 + '0');
-}}
+
+		r = r * 10 + (n % 10);
+
+		n = n / 10;
+
+		ct++;
+
+	}
+
+
+
+	while (r > 0)
+
+	{
+
+		cr = (char)((r % 10) + '0');
+
+		_putchar(cr);
+
+		r = r / 10;
+
+		ct--;
+
+	}
+
+	while (ct != 0)
+
+	{
+
+		_putchar('0');
+
+			ct--;
+
+	}
+
+	_putchar(ld);
+
+}
